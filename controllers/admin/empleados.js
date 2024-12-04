bcbae76a25365bd38011c47f382b9a9402866fac
Ricardo Nicolas Melara <107507393+ElRicki0@users.noreply.github.com>
2024-12-04 15:08:19 -0600

    iconos decorativos e inicio de scrud de empleados

---
+++ controllers/admin/empleados.js
@@ -0,0 +1,6 @@
+// Método del evento para cuando el documento ha cargado.
+document.addEventListener('DOMContentLoaded', async () => {
+    // Llamada a la función para mostrar el encabezado y pie del documento.
+    loadTemplate();
+    MAIN_TITLE.textContent = 'Pagina de empleados';
+});
