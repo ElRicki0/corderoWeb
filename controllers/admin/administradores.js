0318a13bb4df44537bc8268e6b1131c7dd111a90
Ricardo Nicolas Melara <107507393+ElRicki0@users.noreply.github.com>
2024-12-09 19:37:48 -0600

    avance en administradores

---
+++ controllers/admin/administradores.js
@@ -0,0 +1,5 @@
+// ? método cuando el documento ha cargado con éxito 
+document.addEventListener('DOMContentLoaded', ()=>{
+    // ? llama la función para llamar a traer el encabezado del documento
+    loadTemplate();
+});
